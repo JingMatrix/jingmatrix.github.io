d2cbe70f1ce955ccf472aa0ea09d927c70c871d2
JingMatrix <jingmatrix@gmail.com>
2022-09-21 22:00:43 +0200

    Add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './components/**/*.{html,js,vue,ts,md}',
-    './content/**/*.{html,js,vue,ts,md}',
-    './content/.vitepress/**/*.{html,js,vue,ts,md}',
-  ],
+	darkMode: 'class',
+	content: [
+		'./components/**/*.{html,js,vue,ts,md}',
+		'./content/**/*.{html,js,vue,ts,md}',
+		'./content/.vitepress/**/*.{html,js,vue,ts,md}',
+	],
+	plugins: [
+		require('@tailwindcss/typography'),
+	]
 };
