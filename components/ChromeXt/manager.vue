bf4da194c2d15a3c7ed86f458a73904543f92cef
JingMatrix <jingmatrix@gmail.com>
2022-12-27 02:26:42 +0100

    Update scripts by clicking title

--- components/ChromeXt/manager.vue
+++ components/ChromeXt/manager.vue
@@ -1,19 +1,30 @@
 <template>
-	<h1 @click="show_details = ! show_details">{{ header }}</h1>
+	<h1 @click="show_details ? (show_details = false) : getIds()">
+		{{ header }}
+	</h1>
 	<p v-if="scripts.length == 0">
 		{{ translation.introduction }}
 		<a href="https://github.com/JingMatrix/ChromeXt">ChromeXt</a>.
 	</p>
 	<div v-else>
-		<Meta v-if="show_details && script_meta != ''" :meta="script_meta"
-			@exit="show_details=false; script_meta = '';" />
+		<Meta v-if="show_details && script_meta != ''" :meta="script_meta" @exit="
+		  show_details = false;
+		  script_meta = '';
+		" />
 		<div v-else v-for="script in scripts" :key="script" class="pb-4 pl-2 flex flex-row">
-			<font-awesome-icon icon="fa-regular fa-trash-can" class="basis-1/8 mt-1"
-				@click="deleteScriptById([script]);getIds();" />
-			<button class="basis-5/8 flex-1 text-left indent-3" @click="show_details=true;getMetaById([script]);"> {{
-			getName(script) }} </button>
-			<div class="basis-1/4 text-xs px-2 break-all text-gray-200 dark:text-gray-700"> {{
-			getSimpleNameSpace(script) }} </div>
+			<font-awesome-icon icon="fa-regular fa-trash-can" class="basis-1/8 mt-1" @click="
+			  deleteScriptById([script]);
+			  getIds();
+			" />
+			<button class="basis-5/8 flex-1 text-left indent-3" @click="
+			  show_details = true;
+			  getMetaById([script]);
+			">
+				{{ getName(script) }}
+			</button>
+			<div class="basis-1/4 text-xs px-2 break-all text-gray-200 dark:text-gray-700">
+				{{ getSimpleNameSpace(script) }}
+			</div>
 		</div>
 	</div>
 </template>
@@ -21,21 +32,21 @@
 <script lang="ts" setup>
 import { useData } from "vitepress";
 import { ref, onMounted } from "vue";
-import Meta from "./meta.vue"
+import Meta from "./meta.vue";
 
 const show_details = ref(false);
 const script_meta = ref("");
 const scripts = ref([]);
 const translation = useData().frontmatter.value;
-const header = ref(translation.not_installed)
-const SINGLE_QUOTE_ESCAPE = /ChromeXt_Quote_Escape_String/g
+const header = ref(translation.not_installed);
+const SINGLE_QUOTE_ESCAPE = /ChromeXt_Quote_Escape_String/g;
 
 function getIds() {
 	globalThis.ChromeXt(JSON.stringify({ action: "getIds", payload: "" }));
 }
 
 function getMetaById(arg: string[]) {
-	script_meta.value = ""
+	script_meta.value = "";
 	if (show_details.value) {
 		globalThis.ChromeXt(
 			JSON.stringify({ action: "getMetaById", payload: arg })
@@ -45,7 +56,7 @@ function getMetaById(arg: string[]) {
 
 function getName(id: string) {
 	const split = id.split(":");
-	return split[split.length - 1]
+	return split[split.length - 1];
 }
 
 function getSimpleNameSpace(id: string) {
@@ -58,7 +69,7 @@ function getSimpleNameSpace(id: string) {
 			split.shift();
 			let namespace = split.join(":");
 			if (namespace.startsWith("//")) {
-				return namespace.substring(2)
+				return namespace.substring(2);
 			} else {
 				return namespace;
 			}
@@ -75,16 +86,17 @@ function deleteScriptById(arg: string[]) {
 }
 
 onMounted(async () => {
-
 	if (typeof globalThis.ChromeXt !== "undefined") {
-		header.value = translation.installed
+		header.value = translation.installed;
 		window.addEventListener("script_id", (e: CustomEvent) => {
-			scripts.value = e.detail.filter((id: string) => id.includes(":")).
-				map((text: string) => text.replace(SINGLE_QUOTE_ESCAPE, "'"));
-		})
+			scripts.value = e.detail
+				.filter((id: string) => id.includes(":"))
+				.map((text: string) => text.replace(SINGLE_QUOTE_ESCAPE, "'"));
+		});
 		window.addEventListener("script_meta", (e: CustomEvent) => {
-			script_meta.value = e.detail[0].replace(SINGLE_QUOTE_ESCAPE, "`") || "invalid";
-		})
+			script_meta.value =
+				e.detail[0].replace(SINGLE_QUOTE_ESCAPE, "`") || "invalid";
+		});
 		getIds();
 	}
 });
--- components/ChromeXt/meta.vue
+++ components/ChromeXt/meta.vue
@@ -15,34 +15,36 @@
 			:href="metadata.getSupport()">{{ metadata.author }}</a>
 		<div class="my-4" v-for="group in ['match', 'exclude', 'grant']" :key="group">
 			<font-awesome-icon class="pl-2 pr-4 dark:text-gray-600 text-gray-300" icon="fa-solid fa-plus"
-				@click="metadata[group].push('');" />
+				@click="metadata[group].push('')" />
 			<span> {{ capitalizeFirstLetter(group) }} </span>
 			<div v-for="index in metadata[group].length" :key="index"
 				class="flex items-center border-b border-teal-500 py-2">
 				<input
 					class="appearance-none bg-transparent border-none w-full text-gray-500 dark:text-gray-400 mr-3 py-1 px-2 leading-tight focus:outline-none"
-					placeholder="Add new item" v-model="metadata[group][index - 1]">
-				<font-awesome-icon icon="fa-solid fa-xmark" @click="metadata[group].splice(index-1, 1);"
+					placeholder="Add new item" v-model="metadata[group][index - 1]" />
+				<font-awesome-icon icon="fa-solid fa-xmark" @click="metadata[group].splice(index - 1, 1)"
 					class="pr-2 dark:text-gray-600 text-gray-300" />
 			</div>
 		</div>
 		<div class="my-6">
 			<span class="ml-4 mr-12">Run At:</span>
 			<select v-model="metadata.runAt"
-				class="px-1 py-0.5 text-center hover:shadow-md text-gray-500 dark:text-gray-400 bg-inherit dark:bg-gray-900 border-solid border-2 rounded-lg border-slate-400 dark:border-slate-500 hover:border-violet-700 dark:hover:border-violet-100 ">
+				class="px-1 py-0.5 text-center hover:shadow-md text-gray-500 dark:text-gray-400 bg-inherit dark:bg-gray-900 border-solid border-2 rounded-lg border-slate-400 dark:border-slate-500 hover:border-violet-700 dark:hover:border-violet-100">
 				<option v-for="runAt in ['document-start', 'document-idle', 'document-end']" :key="runAt"
 					:value="runAt">
-					<span>{{ capitalizeFirstLetter(runAt.split('-')[1]) }}</span>
+					<span>{{ capitalizeFirstLetter(runAt.split("-")[1]) }}</span>
 				</option>
 			</select>
 		</div>
 		<div class="flex justify-between px-4 text-lg mb-16">
 			<button @click="emit('exit')"
 				class="flex-shrink-0 border-solid border-transparent border-2 text-teal-500 py-1 px-2 rounded">
-				{{ translation.return }} </button>
-			<button @click="metadata.submit();"
+				{{ translation.return }}
+			</button>
+			<button @click="metadata.submit()"
 				class="flex-shrink-0 border-solid border-cyan-400 dark:border-green-800 hover:border-teal-700 border-2 py-1 px-2 rounded">
-				{{ translation.confirm }} </button>
+				{{ translation.confirm }}
+			</button>
 		</div>
 	</div>
 	<p v-else @click="emit('exit')">{{ translation.invalidMeta }}</p>
@@ -50,16 +52,15 @@
 
 <script lang="ts" setup>
 import { useData } from "vitepress";
-import { Metadata } from "./meta"
-import { ref } from "vue"
+import { Metadata } from "./meta";
+import { ref } from "vue";
 
-const props = defineProps<{ meta: string }>()
+const props = defineProps<{ meta: string }>();
 const translation = useData().frontmatter.value;
-const emit = defineEmits(['exit'])
-const metadata = ref(new Metadata(props.meta))
+const emit = defineEmits(["exit"]);
+const metadata = ref(new Metadata(props.meta));
 
 function capitalizeFirstLetter(s: string) {
 	return s.charAt(0).toUpperCase() + s.slice(1);
 }
-
 </script>
