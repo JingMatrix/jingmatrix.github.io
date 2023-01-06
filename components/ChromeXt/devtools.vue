86a2c59b9068b1e97cf26ebd729e4e42969973cb
JingMatrix <jingmatrix@gmail.com>
2023-01-06 17:43:17 +0100

    Only inspect pages

--- components/ChromeXt/devtools.vue
+++ components/ChromeXt/devtools.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
 		<h2 class="mt-16 mb-4 text-xl text-center dark:text-violet-400 text-cyan-600">{{
-		translation.dev_title }}</h2>
+			translation.dev_title
+		}}</h2>
 		<div v-for="p in pages" :key="p.id" class="pl-4 py-3 pr-6">
 			<div class="flex justify-between">
 				<span class="shrink w-64 text-slate-600 dark:text-slate-300">{{ p.title }}</span>
@@ -49,7 +50,9 @@ onMounted(() => {
 	window.addEventListener("pages", (e: CustomEvent) => {
 		pages.value = []
 		e.detail.forEach((it: DevInfo) => {
-			if (!it.url.startsWith("https://chrome-devtools-frontend.appspot.com") && window.location.href != it.url) {
+			if (!it.url.startsWith("https://chrome-devtools-frontend.appspot.com")
+				&& it.type == "page"
+				&& window.location.href != it.url) {
 				pages.value.push(it)
 			}
 		}
