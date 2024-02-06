a466b6ae6ba0c96100c56b9d20e152f34bb57faa
JingMatrix <jingmatrix@gmail.com>
2024-02-06 11:31:18 +0100

    Fix Chinese locale date

--- components/layouts/header.vue
+++ components/layouts/header.vue
@@ -1,54 +1,46 @@
 <template>
-	<div class="relative mb-16" v-if="frontmatter.header">
-		<h1 class="text-3xl text-center pb-16">{{ frontmatter.title }}</h1>
-		<div class="text-sm mb-2 text-cyan-500 hover:text-black">
-			<a v-for="tag in frontmatter.tags"
-				class="mr-4 p-1 rounded-md border-solid border-2 dark:border-gray-600 dark:hover:text-cyan-300"
-				:href="withBase('/tags#' + tag.toLowerCase())">
-				{{ tag }}
-			</a>
-		</div>
-		<span class="text-gray-400 dark:text-gray-600">
-			{{ formatDateByLocale(frontmatter.date, site.lang) }}
-		</span>
-		<Author v-if="frontmatter.author || false" :author="frontmatter.author" />
-	</div>
+  <div class="relative mb-16" v-if="frontmatter.header">
+    <h1 class="text-3xl text-center pb-16">{{ frontmatter.title }}</h1>
+    <div class="text-sm mb-2 text-cyan-500 hover:text-black">
+      <a
+        v-for="tag in frontmatter.tags"
+        class="mr-4 p-1 rounded-md border-solid border-2 dark:border-gray-600 dark:hover:text-cyan-300"
+        :href="withBase('/tags#' + tag.toLowerCase())"
+      >
+        {{ tag }}
+      </a>
+    </div>
+    <span class="text-gray-400 dark:text-gray-600">
+      {{ formatDateByLocale(frontmatter.date, site.lang) }}
+    </span>
+    <Author v-if="frontmatter.author || false" :author="frontmatter.author" />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import Author from "../blog/author.vue";
 import { useData, withBase } from "vitepress";
-// import { onBeforeMount } from "vue";
-
-// onBeforeMount(async () => {
-// 	const { registerSW } = await import("virtual:pwa-register");
-// 	registerSW({
-// 		immediate: true,
-// 		onRegistered() {
-// 			console.info("Service Worker registered");
-// 		},
-// 		onRegisterError(e) {
-// 			console.error("Service Worker registration error!", e);
-// 		},
-// 	});
-// });
 
 const frontmatter = useData().frontmatter;
-
 const site = useData().site;
 
 function formatDateByLocale(date: Date, lang: string) {
-	const options: Intl.DateTimeFormatOptions = {
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-		weekday: "long",
-	};
-	return new Date(date).toLocaleDateString(lang, options);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  };
+  let localDate = new Date(date).toLocaleDateString(
+    lang == "zh" ? "zh-Hans-CN-u-nu-hanidec" : lang,
+    options
+  );
+  if (lang == "zh")
+    localDate = localDate
+      .replaceAll(/一(?=\S[日月])/g, "十")
+      .replace(/二(?=\S[日])/, "二十")
+      .replaceAll("十〇", "十")
+      .replaceAll("日", "日，");
+  return localDate;
 }
 </script>
-
-
-
-
-
