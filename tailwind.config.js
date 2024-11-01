61c2b6532eb2611a8f1955ca23875eb6dd9894b6
JingMatrix <jingmatrix@gmail.com>
2024-11-01 03:10:14 +0100

    Fix build with node 23

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-module.exports = {
-	darkMode: 'class',
-	content: [
-		'./components/**/*.{html,js,vue,ts,md}',
-		'./content/**/*.{html,js,vue,ts,md}',
-		'./content/.vitepress/**/*.{html,js,vue,ts,md}',
-	],
-	plugins: [
-		require('@tailwindcss/typography'),
-	]
+/** @type {import('tailwindcss').Config} */
+
+import typography from "@tailwindcss/typography";
+
+export default {
+  darkMode: "class",
+  content: [
+    "./components/**/*.{html,js,vue,ts,md}",
+    "./content/**/*.{html,js,vue,ts,md}",
+    "./content/.vitepress/**/*.{html,js,vue,ts,md}",
+  ],
+  plugins: [typography],
 };
