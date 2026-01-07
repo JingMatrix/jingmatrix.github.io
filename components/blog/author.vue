17c12ef99ecebf89dc53447d00c2a54464fc3cb1
JingMatrix <jingmatrix@gmail.com>
2026-01-07 17:59:44 +0100

    Improve CSS style of author information block
    Tailwind CSS organizes its styles into three main layers: base, components, and utilities.
    Currently, the style of vitepress is overriding layers style.
    
    We add `!important` to fix the image height overriding, and use
    1. set the main div as a flex container that aligns all its children vertically to the center
    2. explicitly setting the width (w-10) to match the height ensures the image is a perfect circle. object-cover will prevent the image from being distorted if its aspect ratio isn't 1:1.

--- components/blog/author.vue
+++ components/blog/author.vue
@@ -7,9 +7,9 @@ defineProps({
 </script>
 
 <template>
-	<div class="absolute right-0 bottom-0">
+	<div class="absolute right-0 bottom-0 flex items-center">
 		<img v-if="author.avatar" :src="withBase(author.avatar)" :alt="author.name"
-			class="rounded h-10 align-middle aspect-auto inline" />
+			class="rounded !h-10 w-10 object-cover align-middle aspect-auto inline" />
 		<span class="ml-4 mr-2 text-sm text-green-400 dark:text-violet-200">{{ author.name }}</span>
 		<div v-for="link in author.links" class="inline ml-2 mr-2">
 			<a :href="link.url">
